715400727fc8648e0ee3b9de273393706822493e
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2011-12-16 03:05:15 -0600

    Use common versioning
    Change-Id: Ib93d3c285874ddbb9a7ffd81118678828ef82280

--- ev.mk
+++ ev.mk
@@ -19,15 +19,7 @@ PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
 # Set up the product codename, build version & MOTD.
 #
 PRODUCT_CODENAME := Perdo
-#PRODUCT_VERSION_DEVICE_SPECIFIC := p2
-ifeq ($(NIGHTLY_BUILD),true)
-    BUILD_VERSION := 2.0.0p1-$(shell date +%m%d%Y)-NIGHTLY
-else
-    BUILD_VERSION := 2.0.0p1
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.romversion=Evervolv-$(PRODUCT_CODENAME)-$(BUILD_VERSION)
+PRODUCT_VERSION_DEVICE_SPECIFIC := p3
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Google Nexus One\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
@@ -43,9 +35,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.managed.roaming=1 \
     ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
     ro.ril.emc.mode=2
-
-# Hot Reboot overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
 
 # Copy hdpi specific bootanimation
 #PRODUCT_COPY_FILES +=  \
