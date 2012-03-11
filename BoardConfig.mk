eb78b3399738ff60dcbb293beb3bb3f8bd4749ad
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-03-10 23:47:48 -0600

    passion: enable vfp cpu feature
    Change-Id: I194949b9337ea6918a3a1f544e8ba9176817d292

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,8 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+ARCH_ARM_HAVE_VFP := true
+
 # FPU compilation flags
 TARGET_GLOBAL_CFLAGS    += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS  += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
