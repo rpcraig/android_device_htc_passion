0c7260153c530f79e1ea55c3ada62f0c80610444
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-04-16 15:11:28 -0500

    passion: proper defines for v7a neon
    after some digging i realized my previous changes were useless
    all neon specific defines are set by the TARGET_ARCH_VARIANT define with
    the exception of TLS_REGISTER so define it here
    
    Change-Id: Ifb9b84c983a5b6d7c7ba6350463178f30d6c5d0c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,13 +34,10 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := qsd8k
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a8
-TARGET_ARCH_VARIANT_FPU := neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
-ARCH_ARM_HAVE_VFP := true
+TARGET_ARCH_VARIANT         := armv7-a-neon
+TARGET_CPU_ABI              := armeabi-v7a
+TARGET_CPU_ABI2             := armeabi
+ARCH_ARM_HAVE_TLS_REGISTER  := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/htc/passion/include
 
